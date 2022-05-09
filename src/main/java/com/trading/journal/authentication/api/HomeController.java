7fd825d6aaa0db8043d93e249fdbed45b3c9a9bf
Allan Weber <a.cassianoweber@gmail.com>
2022-05-09 20:38:11 +0200

    Test

--- src/main/java/com/trading/journal/authentication/api/HomeController.java
+++ src/main/java/com/trading/journal/authentication/api/HomeController.java
@@ -1,6 +1,7 @@
 package com.trading.journal.authentication.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
@@ -10,6 +11,11 @@ public class HomeController {
 
     @GetMapping
     public Mono<String> home() {
+        return Mono.just("Hello World!");
+    }
+
+    @PostMapping
+    public Mono<String> home2() {
         return Mono.just("Hello World!");
     }
 }
