753e4ff89090b3374699b7bfa021eabeade36cc0
Allan <a.cassianoweber@gmail.com>
2022-06-05 13:29:51 +0200

    Admin user feed

---
+++ src/main/java/com/trading/journal/authentication/user/AdminUserFeedStartup.java
@@ -0,0 +1,34 @@
+package com.trading.journal.authentication.user;
+
+import com.trading.journal.authentication.registration.UserRegistration;
+import com.trading.journal.authentication.user.properties.AdminUserProperties;
+import com.trading.journal.authentication.user.service.ApplicationAdminUserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class AdminUserFeedStartup implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final ApplicationAdminUserService applicationAdminUserService;
+
+    private final AdminUserProperties adminUserProperties;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        applicationAdminUserService.thereIsAdmin()
+                .subscribe(thereIdAdmin -> {
+                    if (thereIdAdmin) {
+                        log.info("Admin has been already create previously");
+                    } else {
+                        UserRegistration userRegistration = new UserRegistration("Admin", "Administrator", "admin", adminUserProperties.email(), null, null);
+                        applicationAdminUserService.createAdmin(userRegistration)
+                                .subscribe(unused -> log.info("Admin user created during startup"));
+                    }
+                });
+    }
+}
