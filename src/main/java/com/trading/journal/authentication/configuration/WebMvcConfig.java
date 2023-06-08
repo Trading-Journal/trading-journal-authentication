e56542749a0899afad0a3fe72c570e23cacc7833
Allan <a.cassianoweber@gmail.com>
2023-06-08 14:34:15 +0200

    Fix pmd issue

--- src/main/java/com/trading/journal/authentication/configuration/WebMvcConfig.java
+++ src/main/java/com/trading/journal/authentication/configuration/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.trading.journal.authentication.configuration;
 
+import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@NoArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
