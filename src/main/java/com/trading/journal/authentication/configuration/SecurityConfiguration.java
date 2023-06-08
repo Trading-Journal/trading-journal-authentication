caf61e361985ed9f536986280caa4517814a8ed2
Allan <a.cassianoweber@gmail.com>
2023-06-08 14:21:35 +0200

    allow all origins pattern and credential

--- src/main/java/com/trading/journal/authentication/configuration/SecurityConfiguration.java
+++ src/main/java/com/trading/journal/authentication/configuration/SecurityConfiguration.java
@@ -12,13 +12,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Map;
 import java.util.stream.Stream;
 
-import static java.util.Arrays.asList;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @RequiredArgsConstructor
@@ -43,7 +41,7 @@ public class SecurityConfiguration {
                 .addFilterBefore(new JwtTokenAuthenticationFilter(jwtTokenAuthenticationCheck), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        httpSecurity.cors().configurationSource(configureCors());
+        httpSecurity.cors(withDefaults());
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
 
@@ -52,21 +50,6 @@ public class SecurityConfiguration {
         httpSecurity.authorizeRequests().antMatchers(getOrganisationAdminPath()).hasAnyAuthority(authorityCategoryMap.get(AuthorityCategory.ORGANISATION));
         httpSecurity.authorizeRequests().anyRequest().hasAnyAuthority(authorityCategoryMap.get(AuthorityCategory.COMMON_USER));
         return httpSecurity.authenticationManager(authenticationManager).build();
-    }
-
-    private CorsConfigurationSource configureCors() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(asList("http://localhost:3000", "https://*.tradefastapp.com"));
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod(HttpMethod.GET);
-        configuration.addAllowedMethod(HttpMethod.POST);
-        configuration.addAllowedMethod(HttpMethod.PATCH);
-        configuration.addAllowedMethod(HttpMethod.PUT);
-        configuration.addAllowedMethod(HttpMethod.DELETE);
-        configuration.addAllowedMethod(HttpMethod.OPTIONS);
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-        return request -> configuration;
     }
 
     private String[] getPublicPath() {
