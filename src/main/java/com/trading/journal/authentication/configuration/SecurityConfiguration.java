c429a1976ecc1759ad69fb31d89bcfa4c2874fd8
Allan <a.cassianoweber@gmail.com>
2023-06-08 09:53:01 +0200

    Configure cors origin

--- src/main/java/com/trading/journal/authentication/configuration/SecurityConfiguration.java
+++ src/main/java/com/trading/journal/authentication/configuration/SecurityConfiguration.java
@@ -13,9 +13,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Map;
 import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
 
 @Configuration
 @RequiredArgsConstructor
@@ -40,7 +43,7 @@ public class SecurityConfiguration {
                 .addFilterBefore(new JwtTokenAuthenticationFilter(jwtTokenAuthenticationCheck), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        httpSecurity.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        httpSecurity.cors().configurationSource(configureCors());
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
 
@@ -49,6 +52,21 @@ public class SecurityConfiguration {
         httpSecurity.authorizeRequests().antMatchers(getOrganisationAdminPath()).hasAnyAuthority(authorityCategoryMap.get(AuthorityCategory.ORGANISATION));
         httpSecurity.authorizeRequests().anyRequest().hasAnyAuthority(authorityCategoryMap.get(AuthorityCategory.COMMON_USER));
         return httpSecurity.authenticationManager(authenticationManager).build();
+    }
+
+    private CorsConfigurationSource configureCors() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(asList("http://localhost:3000/", "https://*.tradefastapp.com/"));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod(HttpMethod.GET);
+        configuration.addAllowedMethod(HttpMethod.POST);
+        configuration.addAllowedMethod(HttpMethod.PATCH);
+        configuration.addAllowedMethod(HttpMethod.PUT);
+        configuration.addAllowedMethod(HttpMethod.DELETE);
+        configuration.addAllowedMethod(HttpMethod.OPTIONS);
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        return request -> configuration;
     }
 
     private String[] getPublicPath() {
