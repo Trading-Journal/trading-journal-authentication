4abaac7e1eb14b44385da2947519eae98e05261d
Allan <a.cassianoweber@gmail.com>
2023-06-08 18:20:52 +0200

    default cors config

--- src/main/java/com/trading/journal/authentication/configuration/SecurityConfiguration.java
+++ src/main/java/com/trading/journal/authentication/configuration/SecurityConfiguration.java
@@ -12,11 +12,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Map;
 import java.util.stream.Stream;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @RequiredArgsConstructor
@@ -41,7 +40,7 @@ public class SecurityConfiguration {
                 .addFilterBefore(new JwtTokenAuthenticationFilter(jwtTokenAuthenticationCheck), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        httpSecurity.cors(withDefaults());
+        httpSecurity.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
 
